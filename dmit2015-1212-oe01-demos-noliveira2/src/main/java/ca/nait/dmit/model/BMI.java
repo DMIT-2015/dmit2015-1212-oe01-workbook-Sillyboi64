e0c92e824dc37e9c0844585a44e9c0de925717fb
Nathan Oliveira <dinoman980@gmail.com>
2022-01-17 17:58:49 -0700

    Second Commit

--- dmit2015-1212-oe01-demos-noliveira2/src/main/java/ca/nait/dmit/model/BMI.java
+++ dmit2015-1212-oe01-demos-noliveira2/src/main/java/ca/nait/dmit/model/BMI.java
@@ -16,7 +16,7 @@ public class BMI {
     }
 
     public String bmiCategory() {
-        if (bmi() <= 18.5){
+        if (bmi() < 18.5){
             return "Underweight";
         }else if(bmi() >= 18.5 && bmi() <= 24.9){
             return "Normal";
