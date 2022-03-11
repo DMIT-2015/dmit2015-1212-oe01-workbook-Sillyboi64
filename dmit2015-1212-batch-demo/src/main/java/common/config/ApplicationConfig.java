c5a1046ef8922e1f5d8654c08c0511149bc62088
Nathan Oliveira <dinoman980@gmail.com>
2022-03-10 21:28:04 -0700

    updated ApplicationConfig to have correct user and pass fields and correct section E01

--- dmit2015-1212-batch-demo/src/main/java/common/config/ApplicationConfig.java
+++ dmit2015-1212-batch-demo/src/main/java/common/config/ApplicationConfig.java
@@ -9,9 +9,9 @@ import jakarta.enterprise.context.ApplicationScoped;
 	@DataSourceDefinition(
 		name="java:app/datasources/mssqlDS",
 		className="com.microsoft.sqlserver.jdbc.SQLServerDataSource",
-		url="jdbc:sqlserver://DMIT-Capstone1.ad.sast.ca;databaseName=DMIT2015_1212_A01_yourNaitUsername;TrustServerCertificate=true",   // change A01 to E01 if you are in section E01, change yourNaitUsername
-		user="yourNaitUsername",  //  change yourNaitUsername
-		password="RemotePassword200012345"),    // Replace 200012345 with your NAIT StudentID
+		url="jdbc:sqlserver://DMIT-Capstone1.ad.sast.ca;databaseName=DMIT2015_1212_E01_noliveira2;TrustServerCertificate=true",   // change A01 to E01 if you are in section E01, change yourNaitUsername
+		user="noliveira2",  //  change yourNaitUsername
+		password="RemotePassword200428099"),    // Replace 200012345 with your NAIT StudentID
 
 })
 
